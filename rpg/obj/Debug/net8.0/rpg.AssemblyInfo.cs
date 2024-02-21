7fe12ee17f9ae3c6eeca2697716dc2b459000067
Pedro Henrique Resende Menezes <pedrohrm050315@hotmail.com>
2024-02-21 11:15:05 -0300

    Commit Pastas

--- rpg/obj/Debug/net8.0/rpg.AssemblyInfo.cs
+++ rpg/obj/Debug/net8.0/rpg.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("rpg")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+67c612f6b9376aa72a5781beddf717876a6c0991")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b555dea0a299d765961d37de8af3c54c2f59bafc")]
 [assembly: System.Reflection.AssemblyProductAttribute("rpg")]
 [assembly: System.Reflection.AssemblyTitleAttribute("rpg")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
